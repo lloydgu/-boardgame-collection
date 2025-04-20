ee303e6c3e9518a20669654ff2277af8555486cb
lloydgu <50546324+lloydgu@users.noreply.github.com>
2025-04-21 03:33:31 +0800

    Add files via upload

--- app.js
+++ app.js
@@ -1,12 +1,6 @@
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-import { faSortAmountDown } from '@fortawesome/free-solid-svg-icons'
-
 const { createApp } = Vue;
 
 createApp({
-    components: {
-        FontAwesomeIcon
-      },
     data() {
         return {
             // 新增排序状态
